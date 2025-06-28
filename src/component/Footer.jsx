62e799f17d46e849ff5b0976aaa17bafe0bea782
Niraj <nirajkm6664@gmail.com>
2025-06-29 05:13:05 +0530

    url server bugs fixed

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,6 +5,7 @@ import {
   FaXTwitter,
   FaYoutube,
 } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { FaApple } from "react-icons/fa";
 import { IoLogoGooglePlaystore } from "react-icons/io5";
